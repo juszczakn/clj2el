820a72ebbd119dd101d54e787a12c18bfb5a0f42
Nick Juszczak <juszczakn@gmail.com>
2015-01-24 17:47:49 -0600

    init

---
+++ macros.el
@@ -0,0 +1,33 @@
+(defun el2clj-convert-arr-to-list (arr)
+  (mapcar (lambda (x) x) arr))
+
+(defmacro fn (param-list body)
+  (list 'lambda (el2clj-convert-arr-to-list param-list) body))
+
+(defmacro def (name body)
+  (list 'setf name body))
+
+(defmacro defn (name param-list &optional body)
+  (let* ((param-list (el2clj-convert-arr-to-list param-list)))
+    (fset name `(lambda ,param-list ,body))))
+
+(defmacro doc (sym)
+  (list 'documentation sym))
+
+(defun el2clj-partition-list (elts)
+  (setf part-list nil)
+  (while (not (equal nil elts))
+    (let ((key (car elts))
+          (val (cadr elts)))
+      (setf part-list (if part-list (cons (list key val) part-list)
+                        (list (list key val))))
+      (setf elts (cddr elts))))
+  (nreverse part-list))
+
+(defmacro el2clj-make-hash-table (&rest contents)
+  (let ((sym (gensym))
+        (pd-contents (el2clj-partition-list contents)))
+    `(list 'progn
+           (quote (setf ,sym (make-hash-table :test 'equal)))
+           (quote (progn ,@(mapcar (lambda (x) (list 'puthash (car x) (cadr x) sym)) pd-contents)))
+           (quote ,sym))))
