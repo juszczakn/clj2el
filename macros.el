80cf2d50af8526acdb7e628f0b137056b6b7e179
Nick Juszczak <juszczakn@gmail.com>
2015-01-25 11:42:53 -0600

    el2clj -> clj2el

--- macros.el
+++ macros.el
@@ -1,20 +1,20 @@
-(defun el2clj-convert-arr-to-list (arr)
+(defun clj2el-convert-arr-to-list (arr)
   (mapcar (lambda (x) x) arr))
 
 (defmacro fn (param-list body)
-  (list 'lambda (el2clj-convert-arr-to-list param-list) body))
+  (list 'lambda (clj2el-convert-arr-to-list param-list) body))
 
 (defmacro def (name body)
   (list 'setf name body))
 
 (defmacro defn (name param-list &optional body)
-  (let* ((param-list (el2clj-convert-arr-to-list param-list)))
+  (let* ((param-list (clj2el-convert-arr-to-list param-list)))
     (fset name `(lambda ,param-list ,body))))
 
 (defmacro doc (sym)
   (list 'documentation sym))
 
-(defun el2clj-partition-list (elts)
+(defun clj2el-partition-list (elts)
   (setf part-list nil)
   (while (not (equal nil elts))
     (let ((key (car elts))
@@ -24,19 +24,19 @@
       (setf elts (cddr elts))))
   (nreverse part-list))
 
-(defmacro el2clj-make-hash-table (&rest contents)
+(defmacro clj2el-make-hash-table (&rest contents)
   (let ((sym (gensym))
-        (pd-contents (el2clj-partition-list contents)))
+        (pd-contents (clj2el-partition-list contents)))
     (list 'progn
           `(setf ,sym (make-hash-table :test 'equal))
           `(progn ,@(mapcar (lambda (x) (list 'puthash (car x) (cadr x) sym)) pd-contents))
           sym)))
 
-(defmacro el2clj-let (param-array &optional body)
-  (list 'let* (el2clj-partition-list (el2clj-convert-arr-to-list param-array))
+(defmacro clj2el-let (param-array &optional body)
+  (list 'let* (clj2el-partition-list (clj2el-convert-arr-to-list param-array))
         body))
 
-(defmacro el2clj-map (f elts)
+(defmacro clj2el-map (f elts)
   (list 'mapcar f elts))
 
 (defmacro str (&rest s)
